edf92c824ba95fc807762e4d86c92e5a9093b93b
Nando118 <fernandoverdysunata0118@gmail.com>
2023-03-10 20:19:09 +0700

    belajar bikin library di composer

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FVS\PHPTestingLibrary;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+}
